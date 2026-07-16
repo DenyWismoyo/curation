d11d27a87283f22b8968fb407b4ad6fb7b5056b3
Deny Wismoyo <deny.wismoyo@gmail.com>
2026-07-16 18:41:55 +0700

    first commit

--- src/components/icon/EcoBirdIcon.tsx
+++ src/components/icon/EcoBirdIcon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconProps } from './types';
+import { IconProps } from '@/types/curation';
 
 export const EcoBirdIcon: React.FC<IconProps> = ({ 
   size = 24, 
--- src/components/icon/HexagonIcons.tsx
+++ src/components/icon/HexagonIcons.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconProps } from './types';
+import { IconProps } from '@/types/curation';
 
 // 1. Hexagon Outline (Kuning)
 export const HexagonOutlineIcon: React.FC<IconProps> = ({ size = 24, className = "", ...props }) => {
